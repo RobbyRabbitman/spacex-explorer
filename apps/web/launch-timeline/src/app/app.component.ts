15740d6f2ca72b8d5bd175ee671c772d1a9489f5
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-06-22 00:54:28 +0200

    web/launch-timeline: launch store test

--- apps/web/launch-timeline/src/app/app.component.ts
+++ apps/web/launch-timeline/src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngxs/store';
+import {
+  GetAllLaunches,
+  LaunchStateModel,
+  launch_state,
+} from '@spacex/launch/data/data-launch';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'launch-timeline-root',
@@ -6,5 +13,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'web-launch-timeline';
+  public readonly launchState$: Observable<LaunchStateModel>;
+  constructor(private readonly store: Store) {
+    store.dispatch(GetAllLaunches);
+    this.launchState$ = store.select<LaunchStateModel>(
+      (state) => state[launch_state]
+    );
+  }
 }
--- apps/web/launch-timeline/src/app/app.module.ts
+++ apps/web/launch-timeline/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { NgxsModule } from '@ngxs/store';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { environment } from '../environments/environment';
+import { DataLaunchModule } from '@spacex/launch/data/data-launch';
+import { SPACEX_API_BASE_URL } from '@spacex/shared/data/data-common';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,8 +18,15 @@ import { environment } from '../environments/environment';
       developmentMode: !environment.production,
     }),
     NgxsLoggerPluginModule.forRoot({ disabled: environment.production }),
+    DataLaunchModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: SPACEX_API_BASE_URL,
+      useValue: 'https://api.spacexdata.com/v4',
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
