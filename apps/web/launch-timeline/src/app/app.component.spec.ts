15a6d1a5f377a2607a4d1aecf43b4f129672c747
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-06-22 01:05:15 +0200

    web/launch-timeline: app test removed boiler code

--- apps/web/launch-timeline/src/app/app.component.spec.ts
+++ apps/web/launch-timeline/src/app/app.component.spec.ts
@@ -16,12 +16,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'web-launch-timeline'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('web-launch-timeline');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
