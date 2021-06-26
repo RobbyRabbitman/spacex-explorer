863392925f934cf39898db23b57edb3bf79f4225
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-06-26 14:53:33 +0200

    launch-timeline: launch overview card align

--- libs/launch-timeline/ui/launch-overview-card/src/lib/launch-overview-card-actions/launch-overview-card-actions.component.ts
+++ libs/launch-timeline/ui/launch-overview-card/src/lib/launch-overview-card-actions/launch-overview-card-actions.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'launch-timeline-ui-launch-overview-card-actions',
   templateUrl: './launch-overview-card-actions.component.html',
   styleUrls: ['./launch-overview-card-actions.component.scss'],
 })
-export class LaunchOverviewCardActionsComponent {}
+export class LaunchOverviewCardActionsComponent {
+  @Input()
+  public align: 'start' | 'end' = 'end';
+}
