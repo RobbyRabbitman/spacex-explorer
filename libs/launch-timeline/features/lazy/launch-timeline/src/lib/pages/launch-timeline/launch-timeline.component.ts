9a749d807cd7e0040f577743ae923b3ff53f370e
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-06-26 14:50:20 +0200

    launch-timeline: sort launches by date (asc)

--- libs/launch-timeline/features/lazy/launch-timeline/src/lib/pages/launch-timeline/launch-timeline.component.ts
+++ libs/launch-timeline/features/lazy/launch-timeline/src/lib/pages/launch-timeline/launch-timeline.component.ts
@@ -1,23 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Select, Store } from '@ngxs/store';
+import { Store } from '@ngxs/store';
 import { GetLaunches, LaunchState } from '@spacex/launch/data/data-launch';
 import { SharedSelectors } from '@spacex/shared/data/data-common';
 import { Launch } from '@spacex/shared/types/launch';
 import { from, Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 import {
   PAGE_LAUNCH_DETAIL,
   QUERY_PARAM_LAUNCH_DETAIL_ID,
 } from '../../routes/routes';
-
+import { sortByKey } from '@spacex/shared/util/util-data';
+import { isNonNull } from '@spacex/shared/util/util-ts';
 @Component({
   selector: 'launch-timeline-launch-timeline',
   templateUrl: './launch-timeline.component.html',
   styleUrls: ['./launch-timeline.component.scss'],
 })
 export class LaunchTimelineComponent implements OnInit {
-  @Select(SharedSelectors.getEntities(LaunchState))
-  public readonly launches$: Observable<Array<Launch>> | undefined;
+  private _launches$: Observable<Array<Launch>> | undefined;
+  public get launches$(): Observable<Array<Launch>> | undefined {
+    return this._launches$;
+  }
 
   constructor(
     private readonly store: Store,
@@ -27,6 +31,12 @@ export class LaunchTimelineComponent implements OnInit {
 
   ngOnInit(): void {
     this.store.dispatch(GetLaunches);
+    this._launches$ = this.store
+      .select(SharedSelectors.getEntities<Launch>(LaunchState))
+      .pipe(
+        filter(isNonNull),
+        map((launches) => sortByKey(launches, 'date_unix', (a, b) => b - a))
+      );
   }
 
   public navigateToLaunchDetail({ id }: Launch): Observable<boolean> {
