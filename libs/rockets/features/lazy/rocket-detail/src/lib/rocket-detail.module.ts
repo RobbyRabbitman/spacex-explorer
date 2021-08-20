efc809fc7195958aebe1c76da3db20cc1a3c4a07
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-08-20 22:13:57 +0200

    rocket-detail: setup

--- libs/rockets/features/lazy/rocket-detail/src/lib/rocket-detail.module.ts
+++ libs/rockets/features/lazy/rocket-detail/src/lib/rocket-detail.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RocketDetailComponent } from './rocket-detail/rocket-detail.component';
 import { RocketDetailRoutingModule } from './routes/rocket-detail-routing.module';
+import { UiImageSliderModule } from '@spacex/shared/ui/ui-image-slider';
 
 @NgModule({
   declarations: [RocketDetailComponent],
-  imports: [CommonModule, RocketDetailRoutingModule],
+  imports: [CommonModule, RocketDetailRoutingModule, UiImageSliderModule],
 })
 export class RocketDetailModule {}
