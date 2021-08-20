63905545f16537ab3e0d696457717ed9e0ed520e
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-08-20 23:15:30 +0200

    rocket-detail: use rocket-engine-card

--- libs/rockets/features/lazy/rocket-detail/src/lib/rocket-detail.module.ts
+++ libs/rockets/features/lazy/rocket-detail/src/lib/rocket-detail.module.ts
@@ -3,9 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RocketDetailComponent } from './rocket-detail/rocket-detail.component';
 import { RocketDetailRoutingModule } from './routes/rocket-detail-routing.module';
 import { UiImageSliderModule } from '@spacex/shared/ui/ui-image-slider';
-
+import { UiRocketEngineCardModule } from '@spacex/rocket/ui/ui-rocket-engine-card';
 @NgModule({
   declarations: [RocketDetailComponent],
-  imports: [CommonModule, RocketDetailRoutingModule, UiImageSliderModule],
+  imports: [
+    CommonModule,
+    RocketDetailRoutingModule,
+    UiImageSliderModule,
+    UiRocketEngineCardModule,
+  ],
 })
 export class RocketDetailModule {}
