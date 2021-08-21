9ace2958e8c183490baf8190444a36d5841ee877
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-08-21 11:12:26 +0200

    shared/types: Unit

--- libs/shared/types/common/src/index.ts
+++ libs/shared/types/common/src/index.ts
@@ -2,5 +2,6 @@ export * from './lib/DatePrecision';
 export * from './lib/Entity';
 export * from './lib/Force';
 export * from './lib/Length';
+export * from './lib/Unit';
 export * from './lib/UUID';
 export * from './lib/Mass';
