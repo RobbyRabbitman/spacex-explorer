e0149844616afc9cf4a9ff2215a4be8f0371bc6d
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-09-20 19:50:51 +0200

    data/theme common module removed

--- libs/shared/data/data-theme/src/lib/data-theme.module.ts
+++ libs/shared/data/data-theme/src/lib/data-theme.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { NgxsModule } from '@ngxs/store';
 import { ThemeState } from './state/ThemeState';
 
 @NgModule({
-  imports: [CommonModule, NgxsModule.forFeature([ThemeState])],
+  imports: [NgxsModule.forFeature([ThemeState])],
 })
 export class DataThemeModule {}
