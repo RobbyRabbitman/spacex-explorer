bc8784e4d4e71fc2659119034024c8fc839f47d5
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-09-20 19:51:14 +0200

    shared/theme detect dark mode

--- libs/shared/features/theme/src/lib/theme.service.ts
+++ libs/shared/features/theme/src/lib/theme.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { Theme } from '@spacex/shared/types/common';
-import { ThemeState } from '@spacex/shared/data/data-theme';
+import { SetTheme, ThemeState } from '@spacex/shared/data/data-theme';
 import { Store } from '@ngxs/store';
 import { Inject } from '@angular/core';
 import { map } from 'rxjs/operators';
@@ -13,7 +13,15 @@ export class ThemeService {
   constructor(
     private readonly store: Store,
     @Inject(DOCUMENT) private readonly document: Document
-  ) {}
+  ) {
+    this.store.dispatch(
+      new SetTheme(
+        document.defaultView?.matchMedia('(prefers-color-scheme: dark)').matches
+          ? Theme.DARK
+          : Theme.LIGHT
+      )
+    );
+  }
 
   public toggleTheme(): Observable<void> {
     return this.store.select(ThemeState.theme).pipe(
