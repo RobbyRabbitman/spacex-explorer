b454411ceb06ce2ec7e213d09b7bba61f53f4a74
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-09-20 20:55:57 +0200

    web rockets: prod credentials firebase

--- apps/web/rockets/src/environments/environment.prod.ts
+++ apps/web/rockets/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyD-nr80z3H1KHR7thK3hjHzKnw_1CSfLPM',
+    authDomain: 'spacex-explorer-681e1.firebaseapp.com',
+    projectId: 'spacex-explorer-681e1',
+    storageBucket: 'spacex-explorer-681e1.appspot.com',
+    messagingSenderId: '224566488819',
+    appId: '1:224566488819:web:4de0e632b9fa602ea65186',
+    measurementId: 'G-LZJ35MYP49',
+  },
 };
