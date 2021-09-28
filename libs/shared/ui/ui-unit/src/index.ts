2dbb7d20880b9aba56114847337964a347569c59
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-09-28 20:07:05 +0200

    util unit: fixed linting

--- libs/shared/ui/ui-unit/src/index.ts
+++ libs/shared/ui/ui-unit/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/ui-unit.module';
 export * from './lib/force/force.component';
 export * from './lib/length/length.component';
 export * from './lib/mass/mass.component';
+export * from './lib/select-unit/select-unit.component';
+export * from './lib/toggle-unit/toggle-unit.component';
