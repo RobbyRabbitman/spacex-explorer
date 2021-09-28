058f0baaf32f252dc715707de6aa2e63f8d53bf2
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-09-28 20:11:52 +0200

    github: icon link refactor

--- libs/shared/features/github/src/lib/github-icon-link/github-icon-link.component.ts
+++ libs/shared/features/github/src/lib/github-icon-link/github-icon-link.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { GITHUB_ICON } from '../shared-features-github.module';
+import { Component } from '@angular/core';
+import { GITHUB_ICON } from '../github.module';
 
 @Component({
   selector: 'spacex-github-icon-link',
   templateUrl: './github-icon-link.component.html',
   styleUrls: ['./github-icon-link.component.scss'],
 })
-export class GithubIconLinkComponent implements OnInit {
-  public icon: string | undefined;
-
-  ngOnInit(): void {
-    this.icon = GITHUB_ICON;
-  }
+export class GithubIconLinkComponent {
+  public readonly icon = GITHUB_ICON;
 }
