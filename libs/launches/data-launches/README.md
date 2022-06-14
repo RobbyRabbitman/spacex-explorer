68501f14c8359f9c59bc5e4034f3c91d58bc294a
RobbyRabbitman <robby.rabbitman@gmail.com>
2022-06-14 23:35:38 +0200

    libs: launches: data-launches generated

---
+++ libs/launches/data-launches/README.md
@@ -0,0 +1,7 @@
+# launches-data-launches
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test launches-data-launches` to execute the unit tests.
---
+++ libs/launches/data-launches/jest.config.ts
@@ -0,0 +1,22 @@
+/* eslint-disable */
+export default {
+  displayName: 'launches-data-launches',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../../coverage/libs/launches/data-launches',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
