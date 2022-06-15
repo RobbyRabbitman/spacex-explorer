8246d85a80c740120d69282fc6b13d8fecdfec34
RobbyRabbitman <robby.rabbitman@gmail.com>
2022-06-15 23:52:05 +0200

    libs: shared: data-common: getter for path

--- libs/shared/data-common/src/lib/api/SpacexApi.ts
+++ libs/shared/data-common/src/lib/api/SpacexApi.ts
@@ -11,15 +11,19 @@ export abstract class SpacexApi<T extends Identifiable> {
   protected readonly api = inject(SPACEX_API);
   protected abstract readonly resource: Resource;
 
+  protected get path() {
+    return `${this.api}/${this.resource}`;
+  }
+
   public all(): Observable<T[]> {
-    return this.client.get<T[]>(`${this.api}/${this.resource}`);
+    return this.client.get<T[]>(this.path);
   }
 
   public one(id: string): Observable<T> {
-    return this.client.get<T>(`${this.api}/${this.resource}/${id}`);
+    return this.client.get<T>(`${this.path}/${id}`);
   }
 
   public query<R>(body: { query: any; options: any }): Observable<R> {
-    return this.client.post<R>(`${this.api}/${this.resource}/query`, body);
+    return this.client.post<R>(`${this.path}/query`, body);
   }
 }
