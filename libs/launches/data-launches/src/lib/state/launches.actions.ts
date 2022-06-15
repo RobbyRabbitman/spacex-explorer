48cd126b60b0d59ee1a7f86c156ea75d84ce8346
RobbyRabbitman <robby.rabbitman@gmail.com>
2022-06-15 22:51:06 +0200

    libs: launches: data-launches: all & one actions

---
+++ libs/launches/data-launches/src/lib/state/launches.actions.ts
@@ -0,0 +1,10 @@
+import { Identifiable, UUID } from '@spacex/shared/types-common';
+
+export class LaunchesAll {
+  public static readonly type = '[Launches] All';
+}
+
+export class LaunchesOne implements Identifiable {
+  public static readonly type = '[Launches] One';
+  public constructor(public readonly id: UUID) {}
+}
--- libs/launches/data-launches/src/lib/state/launches.state.ts
+++ libs/launches/data-launches/src/lib/state/launches.state.ts
@@ -1,8 +1,10 @@
 import { inject, Injectable } from '@angular/core';
-import { State, StateToken } from '@ngxs/store';
+import { Action, State, StateContext, StateToken } from '@ngxs/store';
 import { SpacexState, SpacexStateModel } from '@spacex/shared/data-common';
-import { Launch } from '@spacex/shared/types-common';
+import { Identifiable, Launch } from '@spacex/shared/types-common';
+import { Observable } from 'rxjs';
 import { LaunchesApi } from '../api/launches-api.service';
+import { LaunchesAll, LaunchesOne } from './launches.actions';
 
 export const LAUNCHES_STATE = new StateToken<LaunchesStateModel>('launches');
 
@@ -12,4 +14,19 @@ export type LaunchesStateModel = SpacexStateModel<Launch>;
 @Injectable()
 export class LaunchesState extends SpacexState<Launch> {
   protected readonly api = inject(LaunchesApi);
+
+  @Action(LaunchesAll)
+  protected override all(
+    ctx: StateContext<SpacexStateModel<Launch>>
+  ): Observable<SpacexStateModel<Launch>> {
+    return super.all(ctx);
+  }
+
+  @Action(LaunchesOne)
+  protected override one(
+    ctx: StateContext<SpacexStateModel<Launch>>,
+    identifiable: Identifiable
+  ): Observable<SpacexStateModel<Launch>> {
+    return super.one(ctx, identifiable);
+  }
 }
