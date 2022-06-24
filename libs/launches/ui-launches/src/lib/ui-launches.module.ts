81f2066d0383a929bcd03e74e32581304a3e0123
RobbyRabbitman <robby.rabbitman@gmail.com>
2022-06-24 14:43:21 +0200

    libs: launches: ui-launches: ux+
    fixed h, contain img, loading spinner

--- libs/launches/ui-launches/src/lib/ui-launches.module.ts
+++ libs/launches/ui-launches/src/lib/ui-launches.module.ts
@@ -1,10 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { UiCommonModule } from '@spacex/shared/ui-common';
 import { LaunchCardComponent } from './components/launch-card/launch-card.component';
 
 @NgModule({
-  imports: [CommonModule, MatCardModule],
+  imports: [
+    CommonModule,
+    UiCommonModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+  ],
   declarations: [LaunchCardComponent],
   exports: [LaunchCardComponent],
 })
